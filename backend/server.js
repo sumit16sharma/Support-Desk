e465337dafae51b52bbb382dfb146e2f044facf1
Sumit Sharma <sumit.16noida@gmail.com>
2022-01-28 22:45:38 +0530

    Add Routes and Cantrollers

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('HELLO');
+  res.send('HELLOs');
 });
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
