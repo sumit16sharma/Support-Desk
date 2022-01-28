4a720f82a4aa4d481d063c6985d022e208b68f51
Sumit Sharma <sumit.16noida@gmail.com>
2022-01-28 22:07:36 +0530

    Basic Express Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('HELLO');
+});
+
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
