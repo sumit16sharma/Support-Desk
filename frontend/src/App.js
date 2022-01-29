31a24c5cd54f3a249f2917ee14a06147921f0ca4
Sumit Sharma <sumit.16noida@gmail.com>
2022-01-30 01:46:02 +0530

    Header and Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
 
-function App() {
-  return <div>Hello</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+};
 
 export default App;
