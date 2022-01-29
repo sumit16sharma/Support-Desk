eea099d907209e703ee56b027b9ab0a518638d02
Sumit Sharma <sumit.16noida@gmail.com>
2022-01-30 01:01:57 +0530

    Protect Routes & Authentication

--- backend/cantrollers/userCantroller.js
+++ backend/cantrollers/userCantroller.js
@@ -77,6 +77,19 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Login a User
+// @route   /api/user/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+
+  res.status(200).json(user);
+});
+
 // Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -87,4 +100,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
