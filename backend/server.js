2b480d9715751d4ce61a890c527a24d409d7de28
Sumit Sharma <sumit.16noida@gmail.com>
2022-01-29 11:17:43 +0530

    Error & Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
+
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('HELLOs');
@@ -10,5 +15,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
